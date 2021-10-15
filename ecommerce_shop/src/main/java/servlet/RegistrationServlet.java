9778e4584088e8dca6b45924918a9f620a39f713
dkhmil <0973138620dh@gmail.com>
2021-10-15 19:55:15 +0300

    implement registration servlet

--- ecommerce_shop/src/main/java/servlet/RegistrationServlet.java
+++ ecommerce_shop/src/main/java/servlet/RegistrationServlet.java
@@ -5,7 +5,6 @@ import service.UserService;
 import service.impl.UserServiceImpl;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
