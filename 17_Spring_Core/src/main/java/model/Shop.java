d34d5ff37b5bb17ae0a89b687f0f5d17e1e2fd1b
dkhmil <0973138620dh@gmail.com>
2021-11-08 19:13:20 +0200

    implement ShopDaoImpl

--- 17_Spring_Core/src/main/java/model/Shop.java
+++ 17_Spring_Core/src/main/java/model/Shop.java
@@ -12,4 +12,36 @@ public class Shop {
     private String name;
     private double square;
     private int maxPeople;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSquare() {
+        return square;
+    }
+
+    public void setSquare(double square) {
+        this.square = square;
+    }
+
+    public int getMaxPeople() {
+        return maxPeople;
+    }
+
+    public void setMaxPeople(int maxPeople) {
+        this.maxPeople = maxPeople;
+    }
 }
--- 17_Spring_Core/src/main/java/repository/impl/ShopDaoImpl.java
+++ 17_Spring_Core/src/main/java/repository/impl/ShopDaoImpl.java
@@ -8,29 +8,43 @@ import java.util.List;
 
 public class ShopDaoImpl implements ShopDao {
 
-    private List<Shop> shopList;
+    private final List<Shop> shopList;
 
     public ShopDaoImpl() {
         this.shopList = new ArrayList<Shop>();
     }
 
     public List<Shop> readAll() {
-        return null;
+        return shopList;
     }
 
     public Shop read(int id) {
-        return null;
+        return shopList.stream()
+                .filter(sh -> sh.getId() == id)
+                .findFirst()
+                .orElseGet(Shop::new);
     }
 
     public void create(Shop shop) {
-
+        if (!shopList.contains(shop)) {
+            shopList.add(shop);
+        } else {
+            throw new RuntimeException("Shop already added");
+        }
     }
 
     public void update(Shop shop, String property, String newValue) {
-
+        if (shopList.contains(shop)) {
+            Shop shopToUpdate = read(shop.getId());
+            shopToUpdate.setName(shop.getName());
+            shopToUpdate.setSquare(shop.getSquare());
+            shopToUpdate.setMaxPeople(shop.getMaxPeople());
+        }
     }
 
     public void delete(int id) {
-
+        shopList.stream()
+                .filter(sh -> sh.getId() == id)
+                .forEach(shopList::remove);
     }
 }
