c510791589750f3dc6cd301ca88eba9ab2e8f3e4
dkhmil <0973138620dh@gmail.com>
2021-11-12 19:39:22 +0200

    test methods

--- SpringDataJPA/src/main/java/jpa/App.java
+++ SpringDataJPA/src/main/java/jpa/App.java
@@ -14,9 +14,29 @@ public class App {
         ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
         ShopService shopService = context.getBean(ShopService.class);
 
-        Shop shop = new Shop(1, "Name", 15, 150);
+        Shop shop = new Shop(1, "Shop1", 15, 150);
+        Shop shop2 = new Shop(2, "Shop2", 16, 400);
+        Shop shop3 = new Shop(3, "Shop3", 17, 800);
 
-        shopService.save(shop);
+        //shopService.save(shop);
+        //shopService.save(shop2);
+        //shopService.save(shop3);
 
+
+        //shopService.findAll().forEach(System.out::println);
+
+        // System.out.println(shopService.findById(2));
+/*        shopService.update(shop);
+        shopService.update(shop2);
+        shopService.update(shop3);
+
+        shopService.findAll().forEach(System.out::println);*/
+
+        //System.out.println(shopService.findByName("Shop3"));
+
+        // System.out.println(shopService.findMaxPeopleLessThanEqual(200));
+
+        shopService.delete(3);
+        shopService.findAll().forEach(System.out::println);
     }
 }
