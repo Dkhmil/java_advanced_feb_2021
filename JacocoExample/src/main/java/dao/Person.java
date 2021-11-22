138d7af50ca7da9c82e7dfba40a33b0541314988
dkhmil <0973138620dh@gmail.com>
2021-11-22 19:50:42 +0200

    add dependencies

--- JacocoExample/src/main/java/dao/Person.java
+++ JacocoExample/src/main/java/dao/Person.java
@@ -4,9 +4,6 @@ import lombok.*;
 
 import java.util.Date;
 
-@Data
-
-@NoArgsConstructor
 public class Person {
 
     private String name;
@@ -19,6 +16,38 @@ public class Person {
         this.name = name;
         this.email = email;
         this.password = password;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
 }
--- JacocoExample/src/main/java/service/PersonServiceImp.java
+++ JacocoExample/src/main/java/service/PersonServiceImp.java
@@ -11,6 +11,10 @@ public class PersonServiceImp implements PersonService {
 
     private List<Person> personList;
 
+    public PersonServiceImp(List<Person> personList) {
+        this.personList = personList;
+    }
+
     @Override
     public boolean isAuthorized(Person person) throws NoSuchUserException {
         Person fromList = personList.stream().filter((p) -> p.getName().equals(person.getName())).findFirst()
